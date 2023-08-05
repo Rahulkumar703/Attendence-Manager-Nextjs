90d268e392f3716162eadc45bb02c7e3cb2d82f9
Rahul Kumar <rahulkumar703@outlook.com>
2023-08-05 16:19:36 +0530

    debugging middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,28 +1,35 @@
 import { getToken } from "next-auth/jwt"
 import { NextResponse } from "next/server";
 
-export default middleware = async (req) => {
+const middleware = async (req) => {
+
+    try {
+        const token = await getToken({
+            req,
+            secret: process.env.NEXTAUTH_SECRET,
+        });
 
 
-    const token = await getToken({
-        req,
-        secret: process.env.NEXTAUTH_SECRET,
-    });
+        console.log(process.env.NEXTAUTH_SECRET, "==", token);
 
-    const { pathname } = req.nextUrl;
+        const { pathname } = req.nextUrl;
 
-    const publicUrl = pathname === '/' || pathname === '/login' || pathname === '/signup';
+        const publicUrl = pathname === '/' || pathname === '/login' || pathname === '/signup';
 
-    if (!publicUrl && !token) {
-        return NextResponse.redirect(new URL(`/login?callbackUrl=${pathname}`, req.url));
+        if (!publicUrl && !token) {
+            return NextResponse.redirect(new URL(`/login?callbackUrl=${pathname}`, req.url));
+        }
+
+        if (publicUrl && token) {
+            return NextResponse.redirect(new URL('/dashboard', req.url));
+        }
+
+    } catch (error) {
+        console.error(error);
     }
 
-    if (publicUrl && token) {
-        return NextResponse.redirect(new URL('/dashboard', req.url));
-    }
 
-
-    return NextResponse.next();
+    // return NextResponse.next();
 
 
 }
@@ -35,3 +42,5 @@ export const config = {
         "/profile",
     ]
 }
+
+export default middleware;
