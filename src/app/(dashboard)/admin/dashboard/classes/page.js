abd92c6255782cb68d1d665f3e950aafbaa2f33b
Rahul Kumar <rahulkumar703@outlook.com>
2023-08-13 02:22:13 +0530

    added searchbar

--- src/app/(dashboard)/admin/dashboard/classes/page.js
+++ src/app/(dashboard)/admin/dashboard/classes/page.js
@@ -1,5 +1,4 @@
 "use client"
-import SearchBar from '@/components/SearchBar'
 import styles from '@/styles/admin_dashboard.module.scss'
 import { useRouter } from 'next/navigation'
 import { FiArrowLeft } from 'react-icons/fi'
@@ -14,7 +13,6 @@ export default function Subject() {
                 <FiArrowLeft className={styles.back_btn} size={20} onClick={() => { router.back() }} />
                 <h2>Manage Classes</h2>
             </div>
-            <SearchBar />
         </div>
     )
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import Input from './Input'
 import Button from './Button';
-import { FiSearch } from 'react-icons/fi';
+import { FiSearch, FiX } from 'react-icons/fi';
+import styles from './styles/Searchbar.module.scss'
 
-export default function SearchBar(value, onChange) {
+export default function SearchBar(props) {
+
+    const { value, setValue } = props;
+
+
     return (
-        <div className={style.search_bar}>
-            <Input value={value} onChange={onChange} name={"search"} label={"Search"} />
-            <Button type="button" varrient="filled" className={styles.delete_btn} onClick={() => { handleDelete(sub._id); }}>
-                <FiSearch size={20} />
-            </Button>
+        <div className={`${styles.searchbar} ${value.length ? styles.active : ''}`}>
+            <Input
+                type={"text"}
+                name={"search"}
+                placeholder="Search here"
+                className={styles.searchInput}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+            />
+            <FiX size={18} className={styles.clear_icon} onClick={() => setValue('')} />
         </div>
     )
 }
